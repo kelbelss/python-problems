c6a05832ae736d83ea02340d6bd7b725ab7828f5
kelbelss <124790103+kelbelss@users.noreply.github.com>
2023-10-21 11:07:43 +0200

    Problem 5 - Solved

--- problem_5.py
+++ problem_5.py
@@ -1,3 +1,12 @@
-# 2050  is the smallest number that can be divided by each of the numbers
+# 2520  is the smallest number that can be divided by each of the numbers
 # from 1 to 10 without any remainder. What is the smallest positive number 
 # that is evenly divisible by all of the numbers from 1 to 20?
+
+numbers = []
+
+for i in range(1,240000000):
+    if i % 1 == 0 and i % 2 == 0 and i % 3 == 0 and i % 4 == 0 and i % 5 == 0 and i % 6 == 0 and i % 7 == 0 and i % 8 == 0 and i % 9 == 0 and i % 10 == 0 and i % 11 == 0 and i % 12 == 0 and i % 13 == 0 and i % 14 == 0 and i % 15 == 0 and i % 16 == 0 and i % 17 == 0 and i % 18 == 0 and i % 19 == 0 and i % 20 == 0:
+        numbers.append(i)
+
+print(numbers)        
+       
