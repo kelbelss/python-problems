7ee0f0869afd46947bae4f34f261b2673a619a81
kelbelss <124790103+kelbelss@users.noreply.github.com>
2023-10-30 16:35:21 +0200

    TIC TAC TOE - in progress

---
+++ tic-tac-toe.py
@@ -0,0 +1,41 @@
+print("TIC-TAC-TOE")
+
+
+blocks = [[], [], [], [], [], [], [], [], []]
+
+
+
+# def printGrid (blocks):
+    
+print(blocks[0], blocks[1], blocks[2])
+print(blocks[3], blocks[4], blocks[5])
+print(blocks[6], blocks[7], blocks[8])
+
+
+# user1 = 0
+# user2 = X
+
+user1 = input("User 1: ")
+print(user1)
+
+if user1 == "1":
+    block1 = [0]
+elif user1 == "2":
+    block2 = [0]
+elif user1 == "3":
+    block3 = [0]
+elif user1 == "4":
+    block4 = [0]
+elif user1 == "5":
+    block5 = [0]
+elif user1 == "6":
+    block6 = [0]
+elif user1 == "7":
+    block7 = [0]
+elif user1 == "8":
+    block8 = [0]
+elif user1 == "9":
+    block9 = [0]
+
+
+
