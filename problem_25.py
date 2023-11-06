57639d5e7f075dee8608045a4529bc2a2ada45ae
kelbelss <124790103+kelbelss@users.noreply.github.com>
2023-11-06 14:44:25 +0200

    Problem 25 - Attempt to Solve

--- problem_25.py
+++ problem_25.py
@@ -12,19 +12,30 @@
 # F44 = 701408733
 # F45 = 1134903170
 
-def fibonnaci(n):
+memo = {0: None, 1: 1, 2: 1 }
 
-    if n in [1,2]:
-        return 1
+# dictionary = null, n+1
+
+def fibonnaci(n, memo):
     
-    return fibonnaci (n - 1) + fibonnaci (n - 2)
+    if memo[n] != None:
+        return memo[n]
+    
+    if n == 1 or n == 2:
+        result = 1
+    else:
+        result = fibonnaci(n - 1) + fibonnaci(n - 2)
+    memo[n] = result
+    return result
 
 
 i = 1
-print(fibonnaci(i))
+# print(fibonnaci(i))
 
 
-while fibonnaci(i) <= 10**100:
+# 10**100
+
+while fibonnaci(i) <= 10:
     i += 1
     print(fibonnaci(i))
     print(i)
