58de9e53e4c9e16ee4be197b566a5e80a7e519bd
kelbelss <124790103+kelbelss@users.noreply.github.com>
2024-10-29 21:02:43 +0200

    begin problem

--- Basic DSA/find_primes.py
+++ Basic DSA/find_primes.py
@@ -1,1 +1,17 @@
 # Write a function that returns a list of prime numbers up to a given integer n.
+
+
+
+list_of_factors = []
+
+def factors(number):
+
+    if number > 1:
+        for i in range(1,number):
+            if number % i == 0:
+                list_of_factors.append(i)
+
+factors(24)
+print(list_of_factors)
+
+# 
