67d3b225869f02c1c053d5a20397c86eb3f75b95
kelbelss <124790103+kelbelss@users.noreply.github.com>
2024-10-30 11:24:00 +0200

    improve code and add tests

--- Basic DSA/find_primes.py
+++ Basic DSA/find_primes.py
@@ -1,8 +1,6 @@
 # Write a function that returns a list of prime numbers up to a given integer n.
 
-list_of_primes = []
-
-input = 1297
+input = 3578
 
 def prime(number) -> bool:
     if number > 1:
@@ -11,14 +9,26 @@ def prime(number) -> bool:
                 return False
         return True
                 
+def list_primes(input):
 
-for i in range(1, input+1):
-    if prime(i) == True:
-        list_of_primes.append(i)
+    list_of_primes = []
+
+    for i in range(1, input+1):
+        if prime(i) == True:
+            list_of_primes.append(i)
+            
+    return list_of_primes
     
-print(list_of_primes)
+print(list_primes(input))
 
+def run_tests():
+    assert prime(9) == False
+    assert prime(13) == True
+    assert list_primes(13) == [2, 3, 5, 7, 11, 13]
+    assert list_primes(35) == [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31]
+    print("All tests passed!")
 
+run_tests()
 
 
 
