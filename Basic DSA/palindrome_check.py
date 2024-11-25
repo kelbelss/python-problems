83341353c059f2cb3aa95365d25c36313fa27ee5
kelbelss <124790103+kelbelss@users.noreply.github.com>
2024-11-25 18:27:07 +0200

    begin problem

--- Basic DSA/palindrome_check.py
+++ Basic DSA/palindrome_check.py
@@ -1,1 +1,25 @@
 # Write a function that checks if a given string is a palindrome (reads the same forward and backward).
+
+
+def is_palindrome(word):
+
+        if word[0] == word[-1]:
+            return True
+        else:
+            print("letters being compared", word[0], "and", word[-1])
+            return False
+    
+
+# check size of word in order to work out what must be checked  
+
+
+print(is_palindrome("hello"))
+
+# def run_tests():
+#     assert is_palindrome("madam") == True
+#     assert is_palindrome("flower") == False
+#     assert is_palindrome("racecar") == True
+#     assert is_palindrome("hello") == False
+#     print("All tests passed!")
+
+# run_tests()
